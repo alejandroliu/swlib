2260f496030d691fa72ea2dd9c2abdd78e5663a1
Alejandro Liu <alejandro_liu@hotmail.com>
2025-01-15 12:02:03 +0100

    Preparing v2025.01
    - Added custom geany plugins
    - tweaks and controls

--- src/glibc/glibc.c
+++ src/glibc/glibc.c
@@ -1,7 +1,8 @@
 #define _GNU_SOURCE
 
-#include <stdio.h>
 #include <sched.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/mount.h>
 #include <unistd.h>
 
@@ -9,23 +10,32 @@
 #define SRC "/glibc"
 
 int main(int argc,  char  * const *argv) {
-	const char const *shell[] = { "/bin/sh", NULL };
 
-	if (getuid() == 0) {
-	  fprintf(stderr,"%s: permission denied (do NOT run as root)\n", argv[0]);
-	  return(2);
-	}
+  if (getuid() == 0) {
+    fprintf(stderr,"%s: permission denied (do NOT run as root)\n", argv[0]);
+    return(2);
+  }
 
-	// move glibc stuff in place
-	e("unshare",unshare(CLONE_NEWNS));
-	e("mount",mount(SRC "/usr", "/usr", NULL, MS_BIND, NULL));
-	e("mount",mount(SRC "/var/db/xbps", "/var/db/xbps", NULL, MS_BIND, NULL));
+  const char IN_GLIBC_ENV[] = "IN_GLIBC";
+  char *env_value = getenv(IN_GLIBC_ENV);
+  if (!env_value) {
+    static char IN_GLIBC_TRUE[] = "IN_GLIBC=1";
+    putenv(IN_GLIBC_TRUE);
+    // move glibc stuff in place
+    e("unshare",unshare(CLONE_NEWNS));
+    e("mount",mount(SRC "/usr", "/usr", NULL, MS_BIND, NULL));
+    e("mount",mount(SRC "/var/db/xbps", "/var/db/xbps", NULL, MS_BIND, NULL));
 
-	// drop the rights suid gave us
-	e("setuid",setreuid(getuid(),getuid()));
-	e("setgid",setregid(getgid(),getgid()));
-
-	argv++;
-	if (!argv[0]) argv = (void *)shell;
-	e("execv",execvp(argv[0], argv));
+    // drop the rights suid gave us
+    e("setuid",setreuid(getuid(),getuid()));
+    e("setgid",setregid(getgid(),getgid()));
+  } else {
+    fprintf(stderr,"%s: already in \"glibc\" environment\n", argv[0]);
+  }
+  argv++;
+  if (!argv[0]) {
+    static const char *shell[] = { "/bin/sh", NULL };
+    argv = (void *)shell;
+  }
+  e("execv",execvp(argv[0], argv));
 }
